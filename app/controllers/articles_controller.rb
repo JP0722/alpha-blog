322ef2ebf01e2c142a5d230ab3d63668a1a5247f
Jaya Prakash <pallaprakash07@gmail.com>
2022-11-09 13:14:11 +0530

    Added edit/update functionality to the system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,38 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def create
+		puts "-"*20
+		puts params
+		puts "-"*20
+		@article = Article.new(params.require(:article).permit(:title, :description));
+		if @article.save
+			flash[:notice] = "Article created successully"
+			redirect_to article_path(@article)
+		else
+			puts "*"*20
+			puts @article.errors.full_messages
+			puts "*"*20
+			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title, :description))
+			flash[:notice] = "Article updated successully"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
 end
